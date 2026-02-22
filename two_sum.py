1eaa1929177dbd72ae4675a19a1bf2294f65b167
Rakoshpr <spk.raghavendra@gmail.com>
2026-02-22 21:58:34 +0530

    Implement two-sum solution with brute force and optimized approaches

---
+++ two_sum.py
@@ -0,0 +1,57 @@
+"""Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+"""
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+                
+    def twoSum1(self, nums: list[int], target: int) -> list[int]:
+        num_map = {}
+        for i , num in enumerate(nums):
+            comp = target - num
+            if comp in num_map:
+                return [num_map[comp], i]
+            else:
+                num_map[num] = i
+
+print(Solution().twoSum([2,7,11,15],9))
+print(Solution().twoSum([3,2,4],6))
+print(Solution().twoSum([3,3],6))
+
+print(Solution().twoSum1([2,7,11,15],9))
+print(Solution().twoSum1([3,2,4],6))
+print(Solution().twoSum1([3,3],6))
+
